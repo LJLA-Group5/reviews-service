bec67fdfed28e6078c915909b61a9ccf105e8203
Andrew Kwong <andy.kwong@me.com>
2020-12-04 21:31:05 -0800

    complete PostgreSQL generators

--- db/PostgreSQL/users_generator.js
+++ db/PostgreSQL/users_generator.js
@@ -1,26 +1,31 @@
 /* eslint-disable camelcase */
+require('dotenv').config();
 const fs = require('fs');
 const faker = require('faker');
-const debug = require('debug')('app:gen:psql');
+const debug = require('debug')('app:gen:psql:users');
 const path = require('path');
 
 // 10m users
 const usersStream = fs.createWriteStream(path.join(__dirname, '/data/postgres_users_data.csv'));
-usersStream.write('user_id, username, first_name, photo_url');
-let numberOfUsers = 100;
+usersStream.write('user_id,username,first_name,photo_url\n');
 
-function usersGenerator() {
-  if (numberOfUsers === 0) return usersStream.end();
+function usersGenerator(numberOfUsers) {
+  if (numberOfUsers === 0) {
+    debug('Finished user gen');
+    return usersStream.end();
+  }
   const gender = ['men', 'women'];
   const user_id = numberOfUsers;
   const username = faker.internet.userName();
   const first_name = faker.name.firstName();
-  const photo_url = `https://randomuser.me/api/portraits/thumb/${gender[i % 2]}/${i % 100}.jpg`;
+  const photo_url = `https://randomuser.me/api/portraits/thumb/${gender[numberOfUsers % 2]}/${
+    numberOfUsers % 100
+  }.jpg`;
   const user_entry = `${user_id},${username},${first_name},${photo_url}\n`;
   const streamOkay = usersStream.write(user_entry);
-  numberOfUsers -= 1;
-  if (!streamOkay) usersStream.once('drain', usersGenerator);
-  else usersGenerator();
+  if (!streamOkay) usersStream.once('drain', () => usersGenerator(numberOfUsers - 1));
+  else usersGenerator(numberOfUsers - 1);
 }
 
-usersGenerator();
+debug('users gen start');
+usersGenerator(10000000);
