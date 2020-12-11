153ecc954d267d4f3d572e3c5cbcb37ec55369fa
Andrew Kwong <andy.kwong@me.com>
2020-12-10 21:12:18 -0800

    update gen and schema to include real date

--- db/Cassandra/noUDT/NoUDTGen.js
+++ db/Cassandra/noUDT/NoUDTGen.js
@@ -1,4 +1,4 @@
-/* eslint-disable camelcase */
+/* eslint-disable camelcase,consistent-return */
 require('dotenv').config();
 const fs = require('fs');
 const faker = require('faker');
@@ -10,28 +10,38 @@ const generateRandomNum = (min, max) => Math.floor(Math.random() * (max - min) +
 // // 100m reviews
 let reviewsStream;
 let fileNum = 0;
+const gender = ['men', 'women'];
+const year = ['2015', '2016', '2017', '2018', '2019', '2020'];
+const month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 function reviewsByListing(numberOfReviews) {
   if (numberOfReviews === 0) return reviewsStream.end();
   if (numberOfReviews % 2000000 === 0) {
     fileNum += 1;
-    reviewsStream = fs.createWriteStream(path.join(__dirname, `../data/noUDT_data${fileNum}.csv`));
+    reviewsStream = fs.createWriteStream(
+      path.join(__dirname, `../data/noUDT/noUDT_data${fileNum}.csv`)
+    );
     reviewsStream.write(
-      'listing_id,created_at,accuracy,check_in,cleanliness,communication,location,review_body,review_id,user_first_name,value\n'
+      'listing_id,created_at,accuracy,check_in,cleanliness,communication,location, photo_url,review_body,review_id,user_first_name,value\n'
     );
   }
   const listing_id = generateRandomNum(1, 1000001);
   const review_id = numberOfReviews;
   const user_first_name = faker.name.firstName();
   const review_body = faker.lorem.sentences();
-  const created_at = `${faker.date.between('2015-01-01', '2020-12-05')}`;
+  const created_at = `${year[numberOfReviews % 6]}-${month[numberOfReviews % 12]}-${
+    (numberOfReviews % 29) + 1
+  }`;
   const cleanliness = generateRandomNum(1, 6);
   const communication = generateRandomNum(1, 6);
   const check_in = generateRandomNum(1, 6);
   const accuracy = generateRandomNum(1, 6);
   const location = generateRandomNum(1, 6);
+  const photo_url = `https://randomuser.me/api/portraits/thumb/${gender[numberOfReviews % 2]}/${
+    numberOfReviews % 100
+  }.jpg`;
   const value = generateRandomNum(1, 6);
-  const review_entry = `${listing_id},${created_at},${accuracy},${check_in},${cleanliness},${communication},${location},${review_body},${review_id},${user_first_name},${value}\n`;
+  const review_entry = `${listing_id},${created_at},${accuracy},${check_in},${cleanliness},${communication},${location},${photo_url},${review_body},${review_id},${user_first_name},${value}\n`;
   const streamOkay = reviewsStream.write(review_entry);
   if (!streamOkay) reviewsStream.once('drain', () => reviewsByListing(numberOfReviews - 1));
   else reviewsByListing(numberOfReviews - 1);
