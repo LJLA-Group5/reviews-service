0954242f96c5eaa94318041cfc739a75a4f4382e
Andrew Kwong <andy.kwong@me.com>
2020-12-12 11:58:22 -0800

    edit client to accept new data shape from server

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
+
+function getRandomId(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
 export const options = {
   stages: [
-    { duration: '30s', target: 1500 },
+    // { duration: '10s', target: 1 },
+    // { duration: '15s', target: 10 },
+    // { duration: '20s', target: 100 },
+    // { duration: '25s', target: 1000 },
+    // { duration: '30s', target: 1500 },
+    // { duration: '15s', target: 100 },
+    // { duration: '10s', target: 0 },
+    { duration: '30s', target: 1000 },
     { duration: '45s', target: 1500 },
-    { duration: '10s', target: 0 },
+    { duration: '15s', target: 0 },
   ],
 };
 
 export default function () {
-  let response;
-
-  response = http.get(`http://localhost:3002/api/products/arango/product/${getRandomNum}`);
-  check(response, {
-    'status equals 200': (response) => response.status.toString() === '200',
+  let res = http.get(`http://localhost:3003/api/listings/${getRandomId(1, 10000001)}/reviews/`);
+  check(res, {
+    'status 200': (response) => response.status.toString() === '200',
   });
 
   sleep(1);
--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 require('newrelic');
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
-const reviewController = require('../db/controllers/review.js');
+const debug = require('debug')('app:server:index');
+const reviewController = require('../db/controllers/review');
 
 const port = 3003;
 const app = express();
 
-app.use(morgan('dev'));
 app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '/../client/dist')));
 app.use('/:id', express.static(path.join(__dirname, '/../client/dist')));
@@ -27,4 +26,4 @@ app.patch('/api/listings/:listing_id/reviews/:review_id', reviewController.updat
 // delete review
 app.get('/api/listings/:listing_id/reviews', reviewController.deleteOneReview);
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(port, () => debug(`listening on port ${port}`));
